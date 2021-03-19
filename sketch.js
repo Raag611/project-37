e9fb29dabe7e9c6e45c3867caef0b93dd4398789
Raag611 <73942393+Raag611@users.noreply.github.com>
2021-03-19 15:31:50 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var canva;
+var constantCount
+var gameState = 0
+var database,quiz,question,constestant,qaiz
+function setup(){
+  canvas = createCanvas(850,400);
+  
+}
+
+
+function draw(){
+  background("cyan");
+
+  
+}
